79a0c2517ffd7dad3a3e461968221f815b01c1c4
Gabriel Arruda <108347889+ArrudaG@users.noreply.github.com>
2026-02-19 16:46:29 -0300

    Update reports_service.py

--- src/services/reports_service.py
+++ src/services/reports_service.py
@@ -7,7 +7,9 @@ def salvar_report(conteudo):
     mes = agora.strftime("%m")
     data = agora.strftime("%Y-%m-%d")
 
-    pasta = Path("reports") / ano / mes
+    repo_root = Path(__file__).parent.parent.parent
+    pasta = repo_root ("reports") / ano / mes
+    print(f"Criando pasta: {pasta}")
     pasta.mkdir(parents=True, exist_ok=True)
 
     arquivo = pasta / f"{data}.md"
