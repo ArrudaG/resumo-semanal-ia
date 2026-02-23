ac5c71ae94be6f522da269a272742990920a6516
arrudag <gabrielarr1227@gmail.com>
2026-02-23 14:22:50 -0300

    file: Alteração do email_service.py, antes só era possível enviar o resumo para um email, agora pode enviar para vários emails.

--- src/services/email_service.py
+++ src/services/email_service.py
@@ -1,39 +1,44 @@
 import smtplib
 import logging
 import markdown
+import os
+import json
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from src.config.settings import EMAIL_USER, EMAIL_PASSWORD, EMAIL_TO
+from src.config.settings import EMAIL_USER, EMAIL_PASSWORD
 
 def enviar_email(assunto, mensagem):
-    if not EMAIL_USER or not EMAIL_PASSWORD or not EMAIL_TO:
+    if not EMAIL_USER or not EMAIL_PASSWORD:
         raise ValueError("Credenciais de email não carregadas")
 
     try:
         html_content = markdown.markdown(mensagem)
 
-        msg = MIMEMultipart("alternative")
-        msg["Subject"] = assunto
-        msg["From"] = EMAIL_USER
-        msg["To"] = EMAIL_TO
-
-        part_text = MIMEText(mensagem, "plain", "utf-8")
-
-        part_html = MIMEText(html_content, "html", "utf-8")
-
-        msg.attach(part_text)
-        msg.attach(part_html)
+        emails = json.loads(os.environ["REPORT_EMAILS"])
 
         with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
             server.login(EMAIL_USER, EMAIL_PASSWORD)
-            server.sendmail(
-                EMAIL_USER,
-                EMAIL_TO,
-                msg.as_string()
-            )
-        logging.info("Email enviado com sucesso")
 
-    except Exception as e:
-        print("ERRO SMTP:", e)
-        logging.error(f"Erro ao enviar o email: {e}")
+            for email in emails:
+                msg = MIMEMultipart("alternative")
+                msg["Subject"] = assunto
+                msg["From"] = EMAIL_USER
+                msg["To"] = email
+
+                part_text = MIMEText(mensagem, "plain", "utf-8")
+                part_html = MIMEText(html_content, "html", "utf-8")
+
+                msg.attach(part_text)
+                msg.attach(part_html)
+
+                server.sendmail(
+                    EMAIL_USER,
+                    email,
+                    msg.as_string()
+                )
+                logging.info(f"Email enviado para {email}")
+
+    except Exception as erro:
+        print("ERRO SMTP:", erro)
+        logging.error(f"Erro ao enviar o email: {erro}")
